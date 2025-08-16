5aa677cb52be0dfa0e0c96adaf3bc7eaff599739
Jose Viejo Huerta <jviejo@gmail.com>
2025-08-16 09:55:38 +0200

    new docxmerge

--- notify.sh
+++ notify.sh
@@ -1,4 +1,4 @@
 npm test >salida.txt 2>&1 ; \
-curl -X POST https://millions-inner-james-comp.trycloudflare.com/api/result \
+curl -X POST test.dev.ai.kfs.es/api/result \
   -F "errores=@salida.txt" \
   -F "data={\"githubUrl\":\"$(git config --get remote.origin.url)\"}"
